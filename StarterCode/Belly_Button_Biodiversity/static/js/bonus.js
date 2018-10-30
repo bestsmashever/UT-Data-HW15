7a2f220bb01a8b6a91e36b09f546f7f9dbb929e9
Shawn <dxrshawn@gmail.com>
2018-10-30 00:36:10 -0500

    first commit

---
+++ StarterCode/Belly_Button_Biodiversity/static/js/bonus.js
@@ -0,0 +1,59 @@
+function buildGauge(data){
+    // Enter a speed between 0 and 180
+    console.log(data)
+    var level = data["wfreq"];
+
+    // Trig to calc meter point
+    var degrees = 180 - level/9*180,
+        radius = .5;
+    var radians = degrees * Math.PI / 180;
+    var x = radius * Math.cos(radians);
+    var y = radius * Math.sin(radians);
+
+    // Path: may have to change to create a better triangle
+    var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+        pathX = String(x),
+        space = ' ',
+        pathY = String(y),
+        pathEnd = ' Z';
+    var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+    var data = [{ type: 'scatter',
+    x: [0], y:[0],
+        marker: {size: 28, color:'850000'},
+        showlegend: false,
+        name: 'washing frequency',
+        text: level,
+        hoverinfo: 'text+name'},
+    { values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50],
+    rotation: 90,
+    text: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
+    textinfo: 'text',
+    textposition:'inside',
+    marker: {colors:['rgba(0, 255, 150, .9)', 'rgba(0, 255, 150, .8)', 'rgba(0, 255, 150, .7)',
+                     'rgba(0, 255, 150, .6)', 'rgba(0, 255, 150, .5)', 'rgba(0, 255, 150, .4)',
+                     'rgba(0, 255, 150, .3)', 'rgba(0, 255, 150, .2)', 'rgba(0, 255, 150, .1)',
+                     'rgba(255, 255, 255, 0)']},
+    hole: .5,
+    type: 'pie',
+    showlegend: false
+    }];
+
+    var layout = {
+    shapes:[{
+        type: 'path',
+        path: path,
+        fillcolor: '850000',
+        line: {
+            color: '850000'
+        }
+        }],
+    title: '<b>Belly Button Washing Frequency</b> <br> Scrubs per Week',
+    xaxis: {zeroline:false, showticklabels:false,
+                showgrid: false, range: [-1, 1]},
+    yaxis: {zeroline:false, showticklabels:false,
+                showgrid: false, range: [-1, 1]}
+    };
+
+    Plotly.newPlot('gauge', data, layout);
+}
